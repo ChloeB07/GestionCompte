7b43ba36a879b2036eed5c576e3d17084c7b7bd6
elisarod <60143746+elisarod@users.noreply.github.com>
2020-02-11 14:31:31 +0100

    Update client.java

--- client.java
+++ client.java
@@ -1,5 +1,27 @@
 package entites;
 
 public class client {
-
+	private String nom; 
+	private int code;
+	
+	
+	public client(String nom, int code) {
+		super();
+		this.nom = nom;
+		this.code = code;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	
 }
